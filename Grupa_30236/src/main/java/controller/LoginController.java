ea49318bd5aa44f5c060cc565cfbd1912b03f4ee
flavianmdv <flavianmedvichi123@gmail.com>
2023-11-25 19:46:07 +0200

    initial commit

---
+++ Grupa_30236/src/main/java/controller/LoginController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import model.User;
+import model.validator.Notification;
+import model.validator.UserValidator;
+import service.user.AuthenticationService;
+import view.LoginView;
+
+import java.util.EventListener;
+import java.util.List;
+
+public class LoginController {
+
+    private final LoginView loginView;
+    private final AuthenticationService authenticationService;
+
+
+    public LoginController(LoginView loginView, AuthenticationService authenticationService) {
+        this.loginView = loginView;
+        this.authenticationService = authenticationService;
+
+        this.loginView.addLoginButtonListener(new LoginButtonListener());
+        this.loginView.addRegisterButtonListener(new RegisterButtonListener());
+    }
+
+    private class LoginButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(javafx.event.ActionEvent event) {
+            String username = loginView.getUsername();
+            String password = loginView.getPassword();
+
+            Notification<User> loginNotification = authenticationService.login(username, password);
+
+            if (loginNotification.hasErrors()){
+                loginView.setActionTargetText(loginNotification.getFormattedErrors());
+            }else{
+                loginView.setActionTargetText("LogIn Successfull!");
+            }
+
+        }
+    }
+
+    private class RegisterButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent event) {
+            String username = loginView.getUsername();
+            String password = loginView.getPassword();
+
+            Notification<Boolean> registerNotification = authenticationService.register(username, password);
+
+            if (registerNotification.hasErrors()) {
+                loginView.setActionTargetText(registerNotification.getFormattedErrors());
+            } else {
+                loginView.setActionTargetText("Register successful!");
+            }
+        }
+    }
+}
